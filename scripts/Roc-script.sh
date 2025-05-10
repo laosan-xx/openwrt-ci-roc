5a0a0d642010c0694ab2e38e4b431cdf275096a1
胡说丷刂 <34758853+laosan-xx@users.noreply.github.com>
2025-05-10 21:47:59 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -1,19 +1,12 @@
-# 修改默认IP & 密码 & 固件名称 & 编译署名 & WIFI
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-sed -i 's/root:.*/root:$5$779LrzI9TyqUit1l$7Dwjj6Ysz8RSjBDR.DB5aiumdrWYmDRK0SAr\/xV72C2:20198:0:99999:7:::/g' package/base-files/files/etc/shadow
+# 修改默认IP & 固件名称 & 编译署名
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_generate
 sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Build by Roc')/g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
-sed -i "s/BASE_SSID='LiBwrt'/BASE_SSID='Tk'/" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
-sed -i "s/BASE_WORD='12345678'/BASE_WORD='tk12345678'/" target/linux/qualcommax/base-files/etc/uci-defaults/990_set-wireless.sh
-
-
 
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/net/ddns-scripts
+rm -rf feeds/packages/net/open-app-filter
 rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -26,14 +19,13 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Alist & AdGuardHome & WolPlus & AriaNg & DDNS & 集客无线AC控制器 & Lucky & 雅典娜LED控制
+# AList & AdGuardHome & WolPlus & Lucky & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
+git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git_sparse_clone master https://github.com/immortalwrt/packages net/ariang
-git_sparse_clone openwrt-24.10 https://github.com/immortalwrt/packages net/ddns-scripts
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
+git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
